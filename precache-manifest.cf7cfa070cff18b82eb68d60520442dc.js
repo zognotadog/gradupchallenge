c872ef822d1b7407913db6b124712f12cd3abe3d
Alex Asher <alexasher@hotmail.co.uk>
2020-05-19 20:31:10 +0100

    Updates

---
+++ precache-manifest.cf7cfa070cff18b82eb68d60520442dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11fff35777666a51f8d97baad3a116e",
+    "url": "/gradupchallenge/index.html"
+  },
+  {
+    "revision": "fcec640c7fc6b8aacd0c",
+    "url": "/gradupchallenge/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2b5d20b7270dec5a12f4",
+    "url": "/gradupchallenge/static/css/main.11e463b8.chunk.css"
+  },
+  {
+    "revision": "fcec640c7fc6b8aacd0c",
+    "url": "/gradupchallenge/static/js/2.d50f33b5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gradupchallenge/static/js/2.d50f33b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5d20b7270dec5a12f4",
+    "url": "/gradupchallenge/static/js/main.f2adee23.chunk.js"
+  },
+  {
+    "revision": "8cd91411b39a824b14b7",
+    "url": "/gradupchallenge/static/js/runtime-main.fad5784c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gradupchallenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
